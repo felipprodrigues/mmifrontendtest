d46d5d86b0853e244934f5b686bc168febddf482
felipprodrigues <dev.rodriguesfelipp@gmail.com>
2021-12-03 22:19:36 -0300

    include new features

--- crud/src/components/AddUser.js
+++ crud/src/components/AddUser.js
@@ -1,43 +1,36 @@
 import React, { useState, useContext } from 'react';
-import { GlobalContext } from '../context/GlobalState';
+import { addUser } from '../service/api';
 import { useNavigate } from 'react-router-dom';
 import { v4 as uuid } from 'uuid';
 import { Form, FormFieldset, FormLabel, FormInput } from '../styles/Form.style'
 import { ButtonSubmit, ButtonCancel, ButtonEdit, ButtonReturn } from '../styles/Buttons.style'
 import { MainContainer, MainWrapper, MainFieldset, MainHero } from '../styles/Main.style'
 
+
 export const AddUser = () => {
-  // const [ cnpj, setCnpj ] = useState('');
-  // const [ emprestimo, setEmprestimo ] = useState('');
-  // const [ fatura, setFatura ] = useState('');
-  // const [ address, setAddress ] = useState('');
-  const [ nome, setNome ] = useState('');
-  // const [ cpf, setCpf ] = useState('');
-  // const [ contato, setContato ] = useState('');
-  // const [ email, setEmail ] = useState('');
-
-  const { addUser } = useContext(GlobalContext);
-  const history = useNavigate();
-
-  const onSubmit = () => {
-    const newUser = {
-      id: uuid(),
-      name: nome
-    }
-    addUser(newUser)
-    history('/edit/:id');
+  const initialValues = {
+    nome: '',
+    cpf: '',
+    email: '',
+    telefone: '',
+    cnpj: '',
+    emprestimo: '',
+    faturamento: '',
+    endereco: ''
   }
 
-  const onChange = (el) => {
-    // let element = el.id;
-    // setCnpj(element.target.value);
-    // setEmprestimo(element.target.value);
-    // setFatura(element.target.value);
-    // setAddress(element.target.value);
-    setNome(el.target.value);
-    // setCpf(element.target.value);
-    // setContato(element.target.value);
-    // setEmail(element.target.value);
+  const [user, setUser ] = useState(initialValues);
+  const { nome, cpf, email, telefone, cnpj, emprestimo, faturamento, endereco } = user;
+  const history = useNavigate
+
+  const onValueChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value })
+    console.log(e.target.value);
+    console.log(user)
+  }
+
+  const addUserDetails = async () => {
+    await addUser(user);
   }
 
   return (
@@ -45,41 +38,41 @@ export const AddUser = () => {
       <h1>Cadastre-se</h1>
       <MainWrapper>
         <MainFieldset width="50%">
-          <Form onSubmit={onSubmit} >
-            {/* <FormFieldset>
-              <FormLabel for="cnpj">CNPJ</FormLabel>
-              <FormInput onChange={onChange} id="cnpj" type="text" placeholder="Insira nome de usuário" autoFocus="autofocus" required="required" aria-required="true"></FormInput>
-            </FormFieldset>
-            <FormFieldset>
-              <FormLabel for="emprestimo">Valor do Empréstimo</FormLabel>
-              <FormInput onChange={onChange} id="emprestimo"  type="text" placeholder="Insira nome de usuário" autoFocus="autofocus" required="required" aria-required="true"></FormInput>
-            </FormFieldset>
-            <FormFieldset>
-              <FormLabel for="faturamento">Faturamento Anual da Empresa</FormLabel>
-              <FormInput onChange={onChange} id="faturamento" type="text" placeholder="Insira nome de usuário" autoFocus="autofocus" required="required" aria-required="true"></FormInput>
-            </FormFieldset>
-            <FormFieldset>
-              <FormLabel for="endreço">Endereço da Empresa</FormLabel>
-              <FormInput onChange={onChange} id="endreço"  type="text" placeholder="Insira nome de usuário" autoFocus="autofocus" required="required" aria-required="true"></FormInput>
-            </FormFieldset>*/}
+          <Form>
             <FormFieldset>
               <FormLabel for="nome">Nome</FormLabel>
-              <FormInput onChange={onChange} id="nome" value={nome} type="text" placeholder="Insira nome de usuário" autoFocus="autofocus" required="required" aria-required="true"></FormInput>
-            </FormFieldset>
-            {/* <FormFieldset>
-              <FormLabel for="cpf">CPF do solicitante</FormLabel>
-              <FormInput onChange={onChange} id="cpf"  type="text" placeholder="Insira nome de usuário" autoFocus="autofocus" required="required" aria-required="true"></FormInput>
+              <FormInput onChange={(e) => onValueChange(e)} name="nome" value={nome} type="text" placeholder="Insira nome de usuário" autoFocus="autofocus" ></FormInput>
             </FormFieldset>
             <FormFieldset>
-              <FormLabel for="telefone">Telefone de contato</FormLabel>
-              <FormInput onChange={onChange} id="telefone" type="text" placeholder="Insira nome de usuário" autoFocus="autofocus" required="required" aria-required="true"></FormInput>
+              <FormLabel for="cpf">CPF do solicitante</FormLabel>
+              <FormInput onChange={(e) => onValueChange(e)} name="cpf" value={cpf}  type="text" placeholder="Insira nome de usuário" autoFocus="autofocus" ></FormInput>
             </FormFieldset>
             <FormFieldset>
               <FormLabel for="email">Email para contato</FormLabel>
-              <FormInput onChange={onChange} id="email" type="text" placeholder="Insira nome de usuário" autoFocus="autofocus" required="required" aria-required="true"></FormInput>
-            </FormFieldset> */}
+              <FormInput onChange={(e) => onValueChange(e)} name="email" value={email} type="text" placeholder="Insira nome de usuário" autoFocus="autofocus" ></FormInput>
+            </FormFieldset>
+            <FormFieldset>
+              <FormLabel for="telefone">Telefone de contato</FormLabel>
+              <FormInput onChange={(e) => onValueChange(e)} name="telefone" value={telefone} type="text" placeholder="Insira nome de usuário" autoFocus="autofocus" ></FormInput>
+            </FormFieldset>
+            <FormFieldset>
+              <FormLabel for="cnpj">CNPJ</FormLabel>
+              <FormInput onChange={(e) => onValueChange(e)} name="cnpj" value={cnpj} type="text" placeholder="Insira nome de usuário" autoFocus="autofocus" ></FormInput>
+            </FormFieldset>
+            <FormFieldset>
+              <FormLabel for="emprestimo">Valor do Empréstimo</FormLabel>
+              <FormInput onChange={(e) => onValueChange(e)} name="emprestimo" value={emprestimo} type="text" placeholder="Insira nome de usuário" autoFocus="autofocus" ></FormInput>
+            </FormFieldset>
+            <FormFieldset>
+              <FormLabel for="faturamento">Faturamento Anual da Empresa</FormLabel>
+              <FormInput onChange={(e) => onValueChange(e)} name="faturamento"value={faturamento} type="text" placeholder="Insira nome de usuário" autoFocus="autofocus" ></FormInput>
+            </FormFieldset>
+            <FormFieldset>
+              <FormLabel for="endreço">Endereço da Empresa</FormLabel>
+              <FormInput onChange={(e) => onValueChange(e)} name="endereco" value={endereco} type="text" placeholder="Insira nome de usuário" autoFocus="autofocus" ></FormInput>
+            </FormFieldset>
 
-            <ButtonSubmit type="submit" background="#2E7FED">Enviar</ButtonSubmit>
+            <ButtonSubmit onClick={() => addUserDetails()} background="#2E7FED" >Confirmar</ButtonSubmit>
             <ButtonCancel href="/" background="#EC644B" >Cancelar</ButtonCancel>
 
           </Form>
--- crud/src/components/EditUser.js
+++ crud/src/components/EditUser.js
@@ -1,59 +1,164 @@
-import React, { useState, useContext } from 'react';
-import { GlobalContext } from '../context/GlobalState';
-import { useNavigate } from 'react-router-dom';
-import { v4 as uuid } from 'uuid';
-
-
-import {FormEdit, FormFieldsetEdit, FormLabel, FormInput } from '../styles/Form.style'
-import { ButtonEdit, ButtonCancel, ButtonReturn } from '../styles/Buttons.style'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { TableCell, TableRow, Table, TableHead, TableBody, Button } from '@material-ui/core';
+// import { makeStyles  } from '@material-ui/styles';
+import { getUsers } from '../service/api';
+import { ButtonReturn } from '../styles/Buttons.style'
 import { MainContainer } from '../styles/Main.style'
 
-export const EditUser = (props) => {
-  // const { users, removeUser } = useContext(GlobalContext);
-  // const [ cnpj, setCnpj ] = useState('');
-  // const [ emprestimo, setEmprestimo ] = useState('');
-  // const [ fatura, setFatura ] = useState('');
-  // const [ address, setAddress ] = useState('');
-  const [selectedUser, setSelectedUser] = useState({
-    id: '',
-    name: ''
-  });
-  // const [ cpf, setCpf ] = useState('');
-  // const [ contato, setContato ] = useState('');
-  // const [ email, setEmail ] = useState('');
 
-  const { users, editUser } = useContext(GlobalContext);
-  const history = useNavigate();
-  const currentUserId = props.match.params.id;
+// const useStyles = makeStyles({
+//   table: {
+//     width: '90%',
+//     margin: '50px 0 0 50px'
+//   },
+//   thead: {
+//     '& > *': {
+//       fontSize: 20,
+//       background: '#000000',
+//       color: '#FFFFFF'
+//     }
+//   },
+//   row: {
+//     '& > *': {
+//       fontSize: 18
+//     }
+//   }
+// })
 
-  const onSubmit = () => {
+export const EditUser = () => {
+const [users, setUsers] = useState([]);
+// const classes = useStyles();
 
+useEffect(() => {
+  getAllUsers();
+}, []);
 
-    history('/edit/:id');
-  }
+// const deleteUserData = async (id) => {
+//   await deleteUser(id);
+//   getAllUsers();
+// }
 
-  const onChange = (e) => {
-
-  }
-
-  return (
-    <>
-      <MainContainer>
-        <h1>Editar Dados Cadastrais</h1>
-        {users.map(user => (
-          <FormEdit onSubmit={onSubmit}>
-            <FormFieldsetEdit>
-              <FormLabel>{user.name}</FormLabel>
-              <FormInput onChange={onChange} id="editForm" type="text" placeholder="John Doe" autoFocus="autofocus" required="required" aria-required="true" disabled/>
-            </FormFieldsetEdit>
-            <ButtonEdit href={`/edit/${user.id}`} type="submit" background="#2E7FED">Editar</ButtonEdit>
-            <ButtonCancel  background="#EC644B" >Deletar</ButtonCancel>
-            {/* onClick={() => removeUser(user.id)}  TO GO ON THE BUTTON CANCEL*/}
-          </FormEdit>
-
-        ))}
-        <ButtonReturn href="/add" background="transparent" >Voltar</ButtonReturn>
-      </MainContainer>
-    </>
-  )
+const getAllUsers = async () => {
+  let response = await getUsers();
+  setUsers(response.data);
 }
+
+return (
+  <Table >
+    <TableHead>
+      <TableRow >
+        <TableCell>ID</TableCell>
+        <TableCell>Nome</TableCell>
+        <TableCell>CPF</TableCell>
+        <TableCell>Endereço</TableCell>
+        <TableCell>Email</TableCell>
+        <TableCell>Phone</TableCell>
+        <TableCell>CNPJ</TableCell>
+        <TableCell>Emprestimo</TableCell>
+        <TableCell>Faturamento</TableCell>
+        <TableCell></TableCell>
+      </TableRow>
+    </TableHead>
+    <TableBody>
+      {users.map((user) => (
+        <TableRow >
+          <TableCell>{user.id}</TableCell>
+          <TableCell>{user.name}</TableCell>
+          <TableCell>{user.cpf}</TableCell>
+          <TableCell>{user.endereco}</TableCell>
+          <TableCell>{user.email}</TableCell>
+          <TableCell>{user.phone}</TableCell>
+          <TableCell>{user.cnpj}</TableCell>
+          <TableCell>{user.emprestimo}</TableCell>
+          <TableCell>{user.faturamento}</TableCell>
+          <TableCell>
+            <Button color="primary" variant="contained" style={{ marginRight: 10 }} component={Link} to={`/edit/${user._id}`}>Edit</Button>
+            <Button color="secondary" variant="contained" >Delete</Button>
+          </TableCell>
+        </TableRow>
+      ))}
+    </TableBody>
+  </Table>
+)
+}
+
+
+// onClick={() => deleteUserData(user._id)}
+
+
+// import React, { useState, useEffect } from 'react';
+// import { Table, TableHead, TableCell, Paper, TableRow, TableBody, Button, makeStyles } from '@material-ui/core'
+// import { getUsers, deleteUser } from '../service/api';
+// import { Link } from 'react-router-dom';
+
+// const useStyles = makeStyles({
+//   table: {
+//     width: '90%',
+//     margin: '50px 0 0 50px'
+//   },
+//   thead: {
+//     '& > *': {
+//       fontSize: 20,
+//       background: '#000000',
+//       color: '#FFFFFF'
+//     }
+//   },
+//   row: {
+//     '& > *': {
+//       fontSize: 18
+//     }
+//   }
+// })
+
+
+// const AllUsers = () => {
+//   const [users, setUsers] = useState([]);
+//   const classes = useStyles();
+
+//   useEffect(() => {
+//     getAllUsers();
+//   }, []);
+
+//   const deleteUserData = async (id) => {
+//     await deleteUser(id);
+//     getAllUsers();
+//   }
+
+//   const getAllUsers = async () => {
+//     let response = await getUsers();
+//     setUsers(response.data);
+//   }
+
+//   return (
+//     <Table className={classes.table}>
+//       <TableHead>
+//         <TableRow className={classes.thead}>
+//           <TableCell>Id</TableCell>
+//           <TableCell>Name</TableCell>
+//           <TableCell>Username</TableCell>
+//           <TableCell>Email</TableCell>
+//           <TableCell>Phone</TableCell>
+//           <TableCell></TableCell>
+//         </TableRow>
+//       </TableHead>
+//       <TableBody>
+//         {users.map((user) => (
+//           <TableRow className={classes.row} key={user._id}>
+//             <TableCell>{user._id}</TableCell>
+//             <TableCell>{user.name}</TableCell>
+//             <TableCell>{user.username}</TableCell>
+//             <TableCell>{user.email}</TableCell>
+//             <TableCell>{user.phone}</TableCell>
+//             <TableCell>
+//               <Button color="primary" variant="contained" style={{ marginRight: 10 }} component={Link} to={`/edit/${user._id}`}>Edit</Button>
+//               <Button color="secondary" variant="contained" onClick={() => deleteUserData(user._id)}>Delete</Button>
+//             </TableCell>
+//           </TableRow>
+//         ))}
+//       </TableBody>
+//     </Table>
+//   )
+// }
+
+// export default EditUser;
